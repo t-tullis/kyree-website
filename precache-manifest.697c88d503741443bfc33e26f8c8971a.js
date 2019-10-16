cea9c18c240b068995f8d56a1cbdd08b3c11a3e8
t-tullis <t.tullis34@gmail.com>
2019-10-16 12:36:10 -0700

    Updates

---
+++ precache-manifest.697c88d503741443bfc33e26f8c8971a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1949b940085e64e821d3603bcf10ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b93d7b86fb197c21c8d",
+    "url": "/static/css/2.2615a6d8.chunk.css"
+  },
+  {
+    "revision": "0b6c525e20f3e78b2a89",
+    "url": "/static/css/main.21d4900c.chunk.css"
+  },
+  {
+    "revision": "7b93d7b86fb197c21c8d",
+    "url": "/static/js/2.870b2fbb.chunk.js"
+  },
+  {
+    "revision": "0b6c525e20f3e78b2a89",
+    "url": "/static/js/main.01a76e69.chunk.js"
+  },
+  {
+    "revision": "c4732035d64eeb0f0777",
+    "url": "/static/js/runtime-main.e0579000.js"
+  },
+  {
+    "revision": "98496a5079d25cd1056dfd3a208538b4",
+    "url": "/static/media/GoodMorningAmerica.98496a50.png"
+  },
+  {
+    "revision": "912f0a62694accbd8112018f60593dad",
+    "url": "/static/media/OfficeColor.912f0a62.jpg"
+  },
+  {
+    "revision": "e0b4fa293ae4932e46d143e2629a6b93",
+    "url": "/static/media/archives.e0b4fa29.png"
+  },
+  {
+    "revision": "50ae233fdcfe7ff92a53905d9c9f3390",
+    "url": "/static/media/bloomberg-logo.50ae233f.jpg"
+  },
+  {
+    "revision": "b67e76290dea53bf83e8127080cbea7a",
+    "url": "/static/media/like.b67e7629.png"
+  },
+  {
+    "revision": "925b5da6ab2673ab3ccf2debc4600edf",
+    "url": "/static/media/newspaper.925b5da6.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kyree-website/precache-manifest.0314f3d1edab8ca6343c8adc47b52f95.js"
+  "/precache-manifest.697c88d503741443bfc33e26f8c8971a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kyree-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
